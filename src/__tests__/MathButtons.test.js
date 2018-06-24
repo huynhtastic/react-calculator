c5b4b4b7ece3c839c331edcf3a61067b15e43b67
Richard L Huynh <huynhtastic@gmail.com>
2018-06-24 13:58:51 -0500

    made first experimental unit test

--- src/__tests__/MathButtons.test.js
+++ src/__tests__/MathButtons.test.js
@@ -5,8 +5,12 @@ import App from '../App.js';
 
 configure({ adapter: new Adapter() });
 describe('Testing Math Buttons on App', () => {
+  const app = shallow(<App />);
   it('Testing if the app actually renders', () => {
-    const app = shallow(<App />);
     expect(app.state().currentNumber).toEqual(1);
+  });
+  it('Test if addButton will add', () => {
+    app.find({ id: 'addButton' }).simulate('click');
+    expect(app.state().currentNumber).toEqual(2);
   });
 })
